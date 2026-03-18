e026acfa6ff28715da6ed56e49df6c16bccd586f
David-27 <cleliodavid3@gmail.com>
2026-03-18 09:43:28 +0100

    add client

--- crud.js
+++ crud.js
@@ -1,38 +1,46 @@
-const abirModal = () => document.getElementById("modal")
-.classList.add("active");
+const abirModal = () =>
+  document.getElementById("modal").classList.add("active");
 
-const fecharModal = () => document.getElementById("modal")
-.classList.remove("active");
+const fecharModal = () =>
+  document.getElementById("modal").classList.remove("active");
 
 const btn_cadastrar = document.getElementById("cadastrarCliente");
 
+const btn_canelar = document.getElementById("btn_cancelar");
+
 btn_cadastrar.addEventListener("click", () => {
-    abirModal();
+  abirModal();
 });
 
-btn_cancelar.addEventListener("click", () => {
-    fecharModal();
+btn_canelar.addEventListener("click", () => {
+  fecharModal();
 });
 
 //  FUNCIONALIDADES CRUD
 
 //CRIAR
 
-const clienteTemp = {
-    nome: "Clélio David",
-    email:"cleliodavid3@gmail.com",
-    telefone:"+244 923 420 231",
-    cidade:"Sequele"
-}
+const clienteTemp = [
+  "Clélio David",
+  "cleliodavid3@gmail.com",
+  "+244 923 420 231",
+  "Sequele",
+];
 
 const criarCliente = (client) => {
-    localStorage.setItem("db_cliente", JSON.stringify(client));
-}
+  localStorage.setItem("db_cliente", JSON.stringify(client));
+  clienteTemp.push(client);
+};
+
+const clienteNome = document.getElementById("client_name");
+
+clienteNome.innerHTML = clienteTemp[0];
 
 
 
 
 
 
+const apagarClient = () => {};
 
-
+const editClient = () => {};
